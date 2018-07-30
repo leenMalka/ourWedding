6ca954325c0c19393d365a9e22e4f40a3afcb069
leenMalka <37149321+leenMalka@users.noreply.github.com>
2018-07-30 10:33:12 +0300

    Update wedding.js

--- src/wedding.js
+++ src/wedding.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
       $(".ategoogle").click();
       
     });
-      $(".download").on("click", function() {
-     var a = document.createElement('a');
-  a.href = "http://preview.ibb.co/eF1vOo/Robert_and_Jennifer_2_1.jpg";
-  a.download = "wedding.jpg";
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-  });
+//       $(".download").on("click", function() {
+//      var a = document.createElement('a');
+//   a.href = "http://preview.ibb.co/eF1vOo/Robert_and_Jennifer_2_1.jpg";
+//   a.download = "wedding.jpg";
+//   document.body.appendChild(a);
+//   a.click();
+//   document.body.removeChild(a);
+//   });
       
     });
   
